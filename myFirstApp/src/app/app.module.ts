c8afb151df4b2c58d122f935d0e0a4a834085bfd
Chin Valery <chinvalery@gmail.com>
2020-10-02 19:14:08 +0200

    challenge binding done

--- myFirstApp/src/app/app.module.ts
+++ myFirstApp/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MonComposantComponent } from './mon-composant/mon-composant.component';
-import { MonComposant2Component } from './mon-composant2/mon-composant2.component';
-import { MonComposant3Component } from './mon-composant3/mon-composant3.component';
-import { MonComposant4Component } from './mon-composant4/mon-composant4.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonComposantComponent,
-    MonComposant2Component,
-    MonComposant3Component,
-    MonComposant4Component,
     UserProfileComponent
   ],
   imports: [
